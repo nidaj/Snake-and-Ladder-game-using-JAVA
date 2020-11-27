3beb5fd6f05d2a3997417ad02c5d5bac23fe8f12
nida.j <nida.jawre@gmail.com>
2020-11-27 11:23:33 +0530

    Added code for options of NoPlay Ladder Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,20 +1,63 @@
 import java.util.Random;
 
 public class SnakeAndLadder {
-  	int userPos;
-	int diceRoll;
-  	public SnakeAndLadder(){
-	    	userPos = 0;
+	int userPos;
+	int dice;
+	int option;
+	//int newPos;
+	public SnakeAndLadder(){
+		userPos = 0;
+		System.out.println("User ready to Go....");
 	}
-  	public void rollDie(){
-    		Random rand = new Random();
-		diceRoll = rand.nextInt(5)+1;
-		System.out.println(diceRoll);
-//		userPos = userPos + diceRoll;
-		System.out.println("Die Value: " + diceRoll);
+	static {
+		System.out.println("Welcome to Snakes and Ladders Game");
 	}
+	
+	public void rollDice(){
+		Random rand = new Random();
+		int move;
+		dice = rand.nextInt(5)+1;
+		System.out.println(dice);
+		move = playGame();
+		if(move==1) {
+			userPos = userPos + dice;
+			System.out.println("User At Position: " + userPos);
+		}
+		else if(move==2) {
+//			newPos=userPos-dice;
+			if((userPos-dice)<0) {
+				System.out.println("User back to start");
+				userPos=0;
+			}
+		}
+		else {
+			System.out.println("Stay in place");	
+			}
+		}
+
+  
+	public int playGame() {
+		Random rand = new Random();
+		option = rand.nextInt(3);
+		if(option==0) {
+			System.out.println("No play");
+			return 0;
+		}
+		else if(option==1) {
+			System.out.println("Ladder");
+			return 1;	
+		}
+		else {
+			System.out.println("Snake");
+			return 2;
+		}
+		
+	}
+
 	public static void main(String[] args) {
 		SnakeAndLadder user = new SnakeAndLadder();
-	    	user.rollDie();
+		user.rollDice();
+
 	}
+
 }
